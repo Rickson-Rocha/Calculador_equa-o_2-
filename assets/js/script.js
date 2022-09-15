00f34fadc700a5141fe31ac459a02e72fe51ff0f
Rickson-Rocha <ricksonroccha21@gmail.com>
2022-09-15 01:42:04 -0300

    atualizando script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,34 @@
-function principal(){
+
+const calcular = document.querySelector('#calcular')
+function calc(){
     const a = document.querySelector('#c_a')
     const b = document.querySelector('#c_b')
     const c = document.querySelector('#c_c')
-  
-    const c_a = Number(a.value)
-    const c_b = Number(b.value)
-    const c_c = Number(c.value)
-  
-    const Soma = soma(c_b,c_a)
-    const Produto = produto(c_c,c_a)
-    const msg = document.querySelector('#resultado')
-    msg.innerHTML= `x1=${Soma} x2=${Produto}`
-    
+    const co_a = Number(a.value)
+    const co_b = Number(b.value)
+    const co_c = Number(c.value)
+    const resultado = document.querySelector('#resultado') 
+    if(co_a !='' && co_b !='' && co_c !=''){
+        const x1 = (-(co_b) + raizQuadrada(delta(co_a,co_b,co_c)))/(2*co_a);
+        const x2 = (-(co_b) - raizQuadrada(delta(co_a,co_b,co_c)))/(2*co_a);
+        resultado.innerHTML = `x1=${x1} x2= ${x2}`
+    }else{
+        resultado.innerHTML = 'Para efetuar os calculos é necessário preencher todos os campos'
+    }
 }
 
-function soma(b,a){
-    return -(b/a)
+
+function quadrado(n){
+    return n*n
 }
-function produto(c,a){
-    return(c/a)
+
+function delta(a,b,c){
+
+    return quadrado(b)-(4*a*c);
 }
+
+function raizQuadrada(n){
+   return Math.sqrt(n)
+}
+
+calcular.addEventListener('click',calc)
